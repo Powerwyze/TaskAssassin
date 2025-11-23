9bd3b31930a51d490c177c407aa3b708e6220f9c
juicedarecluse <spc.bstewart@gmail.com>
2025-11-23 11:10:46 -0500

    updated firebase

--- services/firebaseConfig.ts
+++ services/firebaseConfig.ts
@@ -5,13 +5,14 @@ import { getDatabase } from 'firebase/database';
 // TODO: Replace with your Firebase project configuration
 // Get this from Firebase Console > Project Settings > General > Your apps > SDK setup and configuration
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  databaseURL: "https://YOUR_PROJECT_ID-default-rtdb.firebaseio.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyDdHwC9TAjeQBHNJDf2Bd5yx5jzL5sCPuo",
+  authDomain: "gen-lang-client-0549725206.firebaseapp.com",
+  databaseURL: "https://gen-lang-client-0549725206-default-rtdb.firebaseio.com",
+  projectId: "gen-lang-client-0549725206",
+  storageBucket: "gen-lang-client-0549725206.firebasestorage.app",
+  messagingSenderId: "889525001358",
+  appId: "1:889525001358:web:01734050980294e8bbbddf",
+  measurementId: "G-LDFE5LFSMQ"
 };
 
 // Initialize Firebase
